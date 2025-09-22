8de85f127733820429cc1e285b4713776bcdafd4
fsabiu <fsabiu@gmail.com>
2025-09-22 14:01:08 +0000

    changes on env

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Header.css";
-import {logo} from "../../public/images/logo.png";
+
 
 const Header: React.FC = () => {
   return (
